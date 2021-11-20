e31c3691f951a9805aecd63b3cdbbb02a454e970
petiayko <p.makretskii@gmail.com>
2021-11-20 20:44:51 +0300

    v1.3.2 restructured project

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from input import print_answer
 
 # точка входа в программу
 if __name__ == '__main__':
-    filename = 'input1.txt'
+    filename = 'input/input1.txt'
     # чтение данных из файла, запись их в список
     data = [line.replace('\n', '') for line in open(filename)]
     # вызов метода print_answer для печати решения задачи
